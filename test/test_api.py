b77118749f2f3e4763c01cd1e709f4c3b13c350b
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2024-05-04 17:26:38 +0100

    Add test for UDA conversion

--- test/test_api.py
+++ test/test_api.py
@@ -50,6 +50,15 @@ def test_convert_zarr_to_zarr(tmp_path, mapping_files, zarr_file):
     assert Path(target).exists()
 
 
+@pytest.mark.skipif(not uda_available, reason="UDA is not available")
+def test_convert_uda_to_zarr(tmp_path, uda_mapping_files):
+    target = tmp_path
+    mapper = pytokamap.create_mapping(*uda_mapping_files)
+    mapper.to_zarr(30420, target)
+
+    assert Path(target).exists()
+
+
 def test_convert_zarr_to_zarr_delay_compute(tmp_path, mapping_files, zarr_file):
     target = tmp_path
     mapper = pytokamap.create_mapping(*mapping_files)
